e8320dfe831668a76ed60a7397b9c0318ff40ddb
Mark Slingerland <markslingerland@hotmail.com>
2020-08-31 21:28:44 +0200

    Google analytics added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GTAG_TRACKING_ID },
+}, router);
 
 new Vue({
   router,
