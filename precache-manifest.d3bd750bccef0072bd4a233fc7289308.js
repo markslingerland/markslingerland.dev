856f5581a90dd967b2d7e0128bff1f91b38fe3e4
Mark Slingerland <markslingerland@hotmail.com>
2020-08-31 10:01:34 +0200

    Updates

---
+++ precache-manifest.d3bd750bccef0072bd4a233fc7289308.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "b83447436105e656bc07",
+    "url": "/css/app.1a5a10ce.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3b05320428becd1fe1ec0ddb5b79c007",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e885d0e1cd2245568d55",
+    "url": "/js/about.e3175d6b.js"
+  },
+  {
+    "revision": "b83447436105e656bc07",
+    "url": "/js/app.6333aa2b.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "2e14da14c68e3995d852b197accc58ef",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
