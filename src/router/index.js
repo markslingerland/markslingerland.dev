f69da0e09bda3cd87891a5c1acb3d62350502e98
Mark Slingerland <markslingerland@hotmail.com>
2022-04-29 09:55:53 +0200

    Basic setup

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/wedding',
+    name: 'Wedding',
+    // route level code-splitting
+    // this generates a separate chunk (wedding.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "wedding" */ '../views/Wedding.vue'),
+  },
 ];
 
 const router = new VueRouter({
